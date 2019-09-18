c89ba46a3f8bcf4aba57e93c42de51db92d1f56d
AllForTheDea <dragonfleet@gmail.com>
2019-09-18 10:01:04 -0400

    changed again

--- program.rb
+++ program.rb
@@ -5,9 +5,5 @@ puts "I added a minor change"
 
 puts "Not sure this is working"
 
-
-<<<<<<< HEAD
-puts "aaaAmazing new feature! A whole new line!"
-=======
 puts "Amazing new feature! A whole new line!"
->>>>>>> new_feature
+
